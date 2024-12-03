190960386371eee1b282c9c09448d74d3c643f91
OliverIDK <oliveraton125@gmail.com>
2024-12-03 09:13:56 -0600

    reporte

--- screens/Reporte.js
+++ screens/Reporte.js
@@ -1,14 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Dimensions } from "react-native";
+const screenWidth = Dimensions.get("window").width;
+import { LineChart } from "react-native-chart-kit";
+const data = {
+  labels: ["Lun", "Mar", "Mie", "Jue", "Vie", "Sab","Dom"],
+  datasets: [
+    {
+      data: [20, 45, 28, 80, 99, 43, 34],
+      color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+      strokeWidth: 2, // optional
+    },
+  ],
+  legend: ["Ganancias"], // optional
+};
+const chartConfig = {
+  backgroundGradientFrom: "#1E2923",
+  backgroundGradientFromOpacity: 0,
+  backgroundGradientTo: "#08130D",
+  backgroundGradientToOpacity: 0.5,
+  color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, 
+  strokeWidth: 2, // optional, default 3
+  barPercentage: 0.5,
+  useShadowColorFromDataset: false // optional
+};
 const Reporte = () => {
   return (
     <View>
-      <Text>Reporte</Text>
+      <LineChart
+        data={data}
+        width={screenWidth}
+        height={220}
+        chartConfig={chartConfig}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Reporte
+export default Reporte;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
